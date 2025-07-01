ff7d782185a4d50bf5533f792549fc10254f74e5
ariful97768 <ariful97768@gmail.com>
2025-07-02 01:56:47 +0600

    Made Expertise section responsive

--- src/Components/SectionLabel.jsx
+++ src/Components/SectionLabel.jsx
@@ -3,8 +3,8 @@ import { IoIosArrowRoundDown } from "react-icons/io";
 const SectionLabel = ({ text, color }) => {
     return (
         <div className={`flex items-center text-${color}`}>
-            <div className="p-2 border-2 rounded-full"><IoIosArrowRoundDown className="text-2xl" /></div>
-            <p className="px-5 py-2 border-2 rounded-full">{text}</p>
+            <div className="p-1 md:p-2 border-2 rounded-full"><IoIosArrowRoundDown className="text-xl md:text-2xl" /></div>
+            <p className="text-xs md:text-base px-3 md:px-5 py-2 border-2 rounded-full">{text}</p>
         </div>
     );
 };
--- src/Pages/Home/Expertise.jsx
+++ src/Pages/Home/Expertise.jsx
@@ -6,19 +6,19 @@ import { IoIosArrowRoundDown } from "react-icons/io";
 
 const Expertise = () => {
     return (
-        <section className="mx-4 px-16 pb-18 rounded-[40px] pt-23 bg-black text-white">
+        <section className="mx-4 px-8 py-10 md:px-16 md:pb-18 md:pt-23 rounded-3xl md:rounded-4xl lg:rounded-[40px] bg-black text-white">
             <SectionLabel color='white' text={'Why Choose me'} />
-            <div className="flex mt-10 gap-72">
-                <h1 className="text-6xl font-bold leading-21">My Extensive List of Skills</h1>
+            <div className="flex flex-col lg:flex-row gap-8  mt-10 xl:gap-72">
+                <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold leading-10 lg:leading-16 xl:leading-21 text-center lg:text-left">My Extensive List of Skills</h1>
                 <div>
-                    <p className="text-lg leading-9 text-right mb-5">Building the worlds best marketing Your trusted partner for strategy, design, and dev.</p>
-                    <hr className="opacity-45" />
+                    <p className="text-base md:text-lg md:leading-9 text-center lg:text-right mb-5">Building the worlds best marketing Your trusted partner for strategy, design, and dev.</p>
+                    <hr className="opacity-45 hidden lg:block" />
                 </div>
             </div>
-            <div className="grid grid-cols-3 relative pt-16 gap-2">
-                <div className="absolute flex gap-5 text-3xl right-0 -top-10">
-                    <div className="border cursor-pointer rounded-full p-2 rotate-90"><IoIosArrowRoundDown /></div>
-                    <div className="border cursor-pointer rounded-full p-2 -rotate-90"><IoIosArrowRoundDown /></div>
+            <div className="grid lg:grid-cols-3 relative pt-16 gap-2">
+                <div className="absolute flex gap-5 text-3xl right-0 lg:-top-5 xl:-top-10">
+                    <div className="border text-xl md:text-2xl cursor-pointer rounded-full flex items-center justify-center p-2 max-h-max max-w-max rotate-90"><IoIosArrowRoundDown /></div>
+                    <div className="border text-xl md:text-2xl cursor-pointer rounded-full flex items-center justify-center p-2 max-h-max max-w-max -rotate-90"><IoIosArrowRoundDown /></div>
                 </div>
                 <ExpertiseCard icon={<AiOutlineHtml5 />} title={'HTML & CSS'} description={'Skilled in crafting responsive and accessible interfaces using semantic HTML5 and modern CSS techniques.'} />
                 <ExpertiseCard rotate={'rotate-6'} icon={<FaReact />} title={'REACT.JS'} description={'Experienced in building dynamic, component-based UIs with React, using hooks and modern state tools.'} />
@@ -33,11 +33,11 @@ export default Expertise;
 const ExpertiseCard = ({ icon, title, description, rotate }) => {
     return (
         <div className={`${rotate} px-9 py-12 rounded-4xl bg-[#FFFFFF14]`}>
-            <div className="text-8xl">
+            <div className="text-6xl md:text-7xl lg:text-8xl">
                 {icon}
             </div>
-            <h1 className="font-bold text-2xl mt-9 mb-5">{title}</h1>
-            <p>{description}</p>
+            <h1 className="font-bold text-xl md:text-2xl mt-9 mb-5">{title}</h1>
+            <p className="text-sm md:text-base">{description}</p>
         </div>
     )
 }
