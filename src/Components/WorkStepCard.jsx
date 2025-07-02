37725fb1342316464e214179c007406a4c9a16b4
ariful97768 <ariful97768@gmail.com>
2025-07-02 13:44:35 +0600

    Enhance hover effects and add rotateDirection prop to ExpertiseCard. Deployed to firebase hosting.

--- src/Components/WorkStepCard.jsx
+++ src/Components/WorkStepCard.jsx
@@ -2,9 +2,9 @@ import { IoIosArrowRoundDown } from "react-icons/io";
 
 const WorkStep = ({ label, dsc, rotate = false }) => {
     return (
-        <div className={`p-8 pr-9 space-y-11 rounded-4xl ${rotate ? 'rotate-4 z-10 bg-[#C5FF41] text-black':'bg-black'}`}>
+        <div className={`p-8 group pr-9 space-y-11 rounded-4xl ${rotate ? 'rotate-4 z-10 bg-[#C5FF41] text-black':'bg-black'} hover:bg-[#C5FF41] transition-colors duration-700 hover:text-black`}>
             <div className="flex  justify-between">
-                <div className={`${rotate?'bg-black text-white':'bg-[#C5FFEE] text-black'} px-3 md:px-9 py-2 rounded-full md:text-base text-sm`}>{label}</div>
+                <div className={`${rotate?'bg-black text-white':'bg-[#C5FFEE] text-black'} px-3 md:px-9 py-2 rounded-full md:text-base group-hover:bg-black transition-colors duration-700 group-hover:text-white text-sm`}>{label}</div>
                 <div className="flex gap-2 items-center"><IoIosArrowRoundDown className="-rotate-90 hidden md:block text-2xl md:text-4xl" /><a className="underline text-xs md:text-base" href="#">Read More</a></div>
             </div>
             <p className="text-sm">
--- src/Pages/Home/Expertise.jsx
+++ src/Pages/Home/Expertise.jsx
@@ -17,12 +17,12 @@ const Expertise = () => {
             </div>
             <div className="grid lg:grid-cols-3 relative pt-16 gap-2">
                 <div className="absolute flex gap-5 text-3xl right-0 lg:-top-5 xl:-top-10">
-                    <div className="border text-xl md:text-2xl cursor-pointer rounded-full flex items-center justify-center p-2 max-h-max max-w-max rotate-90"><IoIosArrowRoundDown /></div>
-                    <div className="border text-xl md:text-2xl cursor-pointer rounded-full flex items-center justify-center p-2 max-h-max max-w-max -rotate-90"><IoIosArrowRoundDown /></div>
+                    <div className="border transform hover:-translate-x-2 duration-500 text-xl md:text-2xl cursor-pointer rounded-full flex items-center justify-center p-2 max-h-max max-w-max rotate-90"><IoIosArrowRoundDown /></div>
+                    <div className="border transform hover:translate-x-2 duration-500 text-xl md:text-2xl cursor-pointer rounded-full flex items-center justify-center p-2 max-h-max max-w-max -rotate-90"><IoIosArrowRoundDown /></div>
                 </div>
-                <ExpertiseCard icon={<AiOutlineHtml5 />} title={'HTML & CSS'} description={'Skilled in crafting responsive and accessible interfaces using semantic HTML5 and modern CSS techniques.'} />
+                <ExpertiseCard icon={<AiOutlineHtml5 />} title={'HTML & CSS'} rotateDirection={'right'} description={'Skilled in crafting responsive and accessible interfaces using semantic HTML5 and modern CSS techniques.'} />
                 <ExpertiseCard rotate={'rotate-6'} icon={<FaReact />} title={'REACT.JS'} description={'Experienced in building dynamic, component-based UIs with React, using hooks and modern state tools.'} />
-                <ExpertiseCard icon={<LiaDocker />} title={'DOCKER'} description={'Proficient in containerizing applications with Docker for fast deployment, scaling, and environment setup.'} />
+                <ExpertiseCard icon={<LiaDocker />} title={'DOCKER'} rotateDirection={'left'} description={'Proficient in containerizing applications with Docker for fast deployment, scaling, and environment setup.'} />
             </div>
         </section>
     );
@@ -30,9 +30,9 @@ const Expertise = () => {
 
 export default Expertise;
 
-const ExpertiseCard = ({ icon, title, description, rotate }) => {
+const ExpertiseCard = ({ icon, title, description, rotate, rotateDirection }) => {
     return (
-        <div className={`${rotate} px-9 py-12 rounded-4xl bg-[#FFFFFF14]`}>
+        <div className={`${rotate} ${rotateDirection === 'left' && "hover:rotate-3"} ${rotateDirection === 'right' && "hover:-rotate-3"} px-9 py-12 rounded-4xl bg-[#FFFFFF14] transform hover:-translate-y-5 z-50 duration-1000 ease-in-out`}>
             <div className="text-6xl md:text-7xl lg:text-8xl">
                 {icon}
             </div>
